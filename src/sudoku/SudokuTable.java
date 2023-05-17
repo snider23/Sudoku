6475049cd8074859476428226bc5f08f2a502546
szabo_andras <andreas.szabo23@gmail.com>
2023-05-17 18:07:51 +0200

    possible Values

--- src/sudoku/SudokuTable.java
+++ src/sudoku/SudokuTable.java
@@ -1,9 +1,21 @@
 package sudoku;
 
+import java.util.Set;
+
 public class SudokuTable {
     public static final int SUDOKU_SIZE=9;
+    private static final Set<Integer> ALL_POSSIBLE_VALUES = Set.of(1,2,3,4,5,6,7,8,9);
+
+
 
     private int[][] data = new int[SUDOKU_SIZE][SUDOKU_SIZE];
 
+    public Set<Integer> getPossibleValues(int row,int column){
+
+    }
+
+    private Set<Integer> getPossibleInRow(int row){
+        Set<Integer>
+    }
 
 }
