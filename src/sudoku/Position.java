a37bff93ed499110f44f8762caf3fb0d41821eb8
szabo_andras <andreas.szabo23@gmail.com>
2023-05-18 23:35:19 +0200

    mittomen

---
+++ src/sudoku/Position.java
@@ -0,0 +1,5 @@
+package sudoku;
+
+public record Position (int row,int column){
+
+}
--- src/sudoku/SudokuTable.java
+++ src/sudoku/SudokuTable.java
@@ -2,20 +2,26 @@ package sudoku;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 public class SudokuTable {
+
+    private final Random random = new Random();
     public static final int SUDOKU_SIZE = 9;
     public static final int EMPTY_VALUE = 0;
     private static final Set<Integer> ALL_POSSIBLE_VALUES = Set.of(1, 2, 3, 4, 5, 6, 7, 8, 9);
     private int[][] data;
+
     public SudokuTable() {
         this(new int[SUDOKU_SIZE][SUDOKU_SIZE]);
     }
+
     public SudokuTable(int[][] data) {
         validateInputData(data);
         this.data = data;
     }
+
     public Set<Integer> getPossibleValues(int row, int column) {
         Set<Integer> possibleValues = getPossibleValuesInRow(row);
         possibleValues.retainAll(getPossibleValuesInColumn(column));
@@ -42,9 +48,57 @@ public class SudokuTable {
                 row = step.getRow();
                 column = step.getColumn();
             }
-            System.out.println(this);
         }
     }
+
+    public void createPuzzle(int numbersInPuzzle) {
+        generate();
+       Position position= getRandomFilledPosition();
+        clear(position);
+
+        int removedCount = 1;
+
+        while (true) {
+            if (hasCellWithExactlyOnePossibleValue()){
+                if(removedCount == SUDOKU_SIZE * SUDOKU_SIZE - numbersInPuzzle){
+                    break;
+                }
+                position= getRandomFilledPosition();
+                clear(position);
+            }else {
+
+            }
+        }
+    }
+
+    private Position getRandomFilledPosition(){
+
+
+        while (true){
+            int row = random.nextInt(8);
+            int column = random.nextInt(8);
+            if (data[row][column]!=EMPTY_VALUE){
+                return new Position(row,column);
+            }
+        }
+    }
+
+    private void clear(Position position){
+        data[position.row()][position.column()]=EMPTY_VALUE;
+    }
+
+    private boolean hasCellWithExactlyOnePossibleValue() {
+        for (int row = 0; row < SUDOKU_SIZE; row++) {
+            for (int column = 0; column < SUDOKU_SIZE; column++) {
+                if (data[row][column]==EMPTY_VALUE&& getPossibleValues(row,column).size()==1){
+                return true;
+                }
+            }
+
+        }
+        return false;
+    }
+
 
     @Override
     public String toString() {
@@ -55,6 +109,7 @@ public class SudokuTable {
         }
         return builder.toString();
     }
+
 
     private boolean allCellsAreFilled() {
         for (int row = 0; row < SUDOKU_SIZE; row++) {
@@ -74,6 +129,7 @@ public class SudokuTable {
         }
         return result;
     }
+
     private Set<Integer> getPossibleValuesInColumn(int column) {
         Set<Integer> result = new HashSet<>(ALL_POSSIBLE_VALUES);
         for (int i = 0; i < SUDOKU_SIZE; i++) {
@@ -81,6 +137,7 @@ public class SudokuTable {
         }
         return result;
     }
+
     private Set<Integer> getPossibleValuesInZone(int zone) {
         Set<Integer> result = new HashSet<>(ALL_POSSIBLE_VALUES);
         int column = (zone % 3) * 3;
@@ -92,9 +149,11 @@ public class SudokuTable {
         }
         return result;
     }
+
     private int convertRowAndColumToZone(int row, int column) {
         return (row / 3) * 3 + (column / 3);
     }
+
     private void validateInputData(int[][] data) {
         if (data.length != SUDOKU_SIZE) {
             throw new IllegalArgumentException("Sudoku size must be 9x9");
@@ -104,4 +163,5 @@ public class SudokuTable {
                 throw new IllegalArgumentException("Sudoku size must be 9x9");
             }
         }
-    }}
+    }
+}
